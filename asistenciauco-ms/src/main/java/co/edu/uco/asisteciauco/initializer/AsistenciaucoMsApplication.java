a2a320f7c096665a19b482347c8d7e8d43750c76
Juan Pablo <jpabloquin@gmail.com>
2025-04-17 19:09:54 -0500

    Cargar lineal base
    Cargar arquetipo inicial de la lineal base para comenzar a evolucionar.

---
+++ asistenciauco-ms/src/main/java/co/edu/uco/asisteciauco/initializer/AsistenciaucoMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.asisteciauco.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciaucoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciaucoMsApplication.class, args);
+	}
+
+}
---
+++ asistenciauco-ms/src/test/java/co/edu/uco/asisteciauco/initializer/AsistenciaucoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.asisteciauco.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaucoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
